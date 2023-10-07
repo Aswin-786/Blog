5ebafcdb9976b043d5f0f08e7c7d80140fe0c640
Aswin <aswinkrishna363@gmail.com>
2023-10-07 11:48:28 +0530

    changed inputs into mui textfields

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { TextField } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
   const [username, setUsername] = useState("");
@@ -7,7 +9,6 @@ const RegisterPage = () => {
 
   async function register(e) {
     e.preventDefault();
-
     try {
       const response = await axios.post(
         "http://localhost:4000/register",
@@ -32,22 +33,38 @@ const RegisterPage = () => {
     }
   }
   return (
-    <form className="register" onSubmit={register}>
-      <h1>Register</h1>
-      <input
-        type="text"
+    <form
+      className="register  flex flex-col gap-4 items-center "
+      onSubmit={register}
+    >
+      <h1 className="text-center font-semibold text-2xl py-5">Register</h1>
+      <TextField
+        required
+        id="outlined-basic"
+        label="UserName"
+        color="primary"
+        variant="outlined"
+        className="w-full focus-visible:outline-black"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
-        placeholder="username"
       />
-      <input
+      <TextField
+        required
+        id="outlined-password-input"
+        label="Password"
         type="password"
-        id=""
-        placeholder="password"
+        autoComplete="current-password"
+        className="w-full"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
       <button>Register</button>
+      <h3>
+        Allready registered?{" "}
+        <Link to={"/login"} className="text-stone-600 underline text-lg">
+          Login
+        </Link>
+      </h3>
     </form>
   );
 };
