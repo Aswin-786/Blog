2505540e6bc557ab7756de84ac682d0c2c08308e
Aswin <aswinkrishna363@gmail.com>
2023-10-15 18:54:20 +0530

    created custom hook for login

---
+++ client/src/hooks/useLogin.ts
@@ -0,0 +1,62 @@
+import { ChangeEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { userState } from "../store/atoms/User";
+import { useSetRecoilState } from "recoil";
+import axios from "axios";
+
+const useLogin = () => {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
+
+  const setUser = useSetRecoilState(userState);
+
+  const handleSetUsername = (e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+  const handlesetPassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const login = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/login",
+        {
+          username,
+          password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+      if (response.status === 200) {
+        setUser({
+          isLoading: false,
+          userName: response.data.username,
+          userId: response.data.id,
+        });
+        navigate("/");
+      } else {
+        alert("Wrong credentials");
+      }
+    } catch (error) {
+      setError("error occurs");
+    }
+  };
+  return {
+    login,
+    handleSetUsername,
+    username,
+    password,
+    handlesetPassword,
+    error,
+  };
+};
+
+export default useLogin;
--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,79 +1,58 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 import TextField from "@mui/material/TextField";
-import axios from "axios";
-import { userState } from "../store/atoms/User";
-import { useSetRecoilState } from "recoil";
+import useLogin from "../hooks/useLogin";
 import ErrorPage from "./ErrorPage";
-
 const LoginPage: React.FC = () => {
-  const [username, setUsername] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const navigate = useNavigate();
+  const {
+    login,
+    error,
+    username,
+    password,
+    handleSetUsername,
+    handlesetPassword,
+  } = useLogin();
 
-  const setUser = useSetRecoilState(userState);
-
-  async function login(e: React.FormEvent) {
-    e.preventDefault();
-    try {
-      const response = await axios.post(
-        "http://localhost:4000/login",
-        {
-          username,
-          password,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-          withCredentials: true,
-        }
-      );
-      if (response.status === 200) {
-        setUser({
-          isLoading: false,
-          userName: response.data.username,
-          userId: response.data.id,
-        });
-        navigate("/");
-      } else {
-        alert("Wrong credentials");
-      }
-    } catch (error) {
-      return <ErrorPage />;
-    }
-  }
   return (
-    <form className="login flex flex-col gap-4 items-center " onSubmit={login}>
-      <h1 className="text-center font-semibold text-2xl py-5">Login</h1>
-      <TextField
-        required
-        id="outlined-basic"
-        label="UserName"
-        color="primary"
-        variant="outlined"
-        className="w-full focus-visible:outline-black"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <TextField
-        required
-        id="outlined-password-input"
-        label="Password"
-        type="password"
-        autoComplete="current-password"
-        className="w-full"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button>Login</button>
-      <h3>
-        Don't you have account?{" "}
-        <Link to={"/register"} className="text-stone-600 underline">
-          Register
-        </Link>
-      </h3>
-    </form>
+    <>
+      {error ? (
+        <ErrorPage />
+      ) : (
+        <form
+          className="login flex flex-col gap-4 items-center "
+          onSubmit={login}
+        >
+          <h1 className="text-center font-semibold text-2xl py-5">Login</h1>
+          <TextField
+            required
+            id="outlined-basic"
+            label="UserName"
+            color="primary"
+            variant="outlined"
+            className="w-full focus-visible:outline-black"
+            value={username}
+            onChange={handleSetUsername}
+          />
+          <TextField
+            required
+            id="outlined-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            className="w-full"
+            value={password}
+            onChange={handlesetPassword}
+          />
+          <button>Login</button>
+          <h3>
+            Don't you have account?{" "}
+            <Link to={"/register"} className="text-stone-600 underline">
+              Register
+            </Link>
+          </h3>
+        </form>
+      )}
+    </>
   );
 };
 
