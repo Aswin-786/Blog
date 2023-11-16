59f37aa1b63272e00c2cb1b16271a979c9721c24
Aswin <122090298+Aswin-786@users.noreply.github.com>
2023-11-16 12:10:12 +0530

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -27,6 +27,9 @@ if (!mongoUrl) {
 }
 mongoose.connect(mongoUrl);
 
+app.get('/',(req,res)=> {
+  res.status(200).json({message:'hihi'})
+
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
 });
