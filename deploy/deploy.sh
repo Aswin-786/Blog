fe31ba6e7fe46925443b3bc7b1d9b96aad195ce3
Aswin <aswinkrishna363@gmail.com>
2023-11-16 16:20:01 +0530

    updated

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,16 +2,13 @@
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
 
 cd Blog
- git stash
- git pull origin main
- git stash apply
- curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
- export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
- nvm install node
- npm i -g pm2
- npm install -g typescript
+#  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+#  export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+#  nvm install node
+#  npm i -g pm2
+#  npm install -g typescript
  cd api
  npm i
  git stash
